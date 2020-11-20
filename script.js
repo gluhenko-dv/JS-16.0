8f534ed858752d150ec077d1316d3e3bd17c0e5a
Gluhenko Daniil <gluhenko-dv@yandex.ru>
2020-11-20 15:41:22 +0300

    Lesson 2 hard

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 "use strict";
 
 let num = 266219,
-    prod = 1;
+  prod = 1;
 
-console.log(num.toString().split('').reduce(((acc, I) => acc * Number.parseInt(I, 10)), 1));
+console.log(num.toString().split('').reduce(((acc, i) => acc * Number.parseInt(i, 10)), 1));
 
 while (num > 0) {
-    prod *= num % 10;
-    num = Math.floor(num / 10);
+  prod *= num % 10;
+  num = Math.floor(num / 10);
 }
 console.log(prod);
