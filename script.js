74b8489757231c29884a94652294c596c33f403b
Gluhenko Daniil <gluhenko-dv@yandex.ru>
2020-11-21 09:12:42 +0300

    Lesson 2 hard

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 "use strict";
 
-let num = 2,
+let num = 266219,
   prod = 1;
-
+//1 способ
 console.log(num.toString().split('').reduce((acc, i) => acc * i));
 
+//2 способ
 while (num > 0) {
   prod *= num % 10;
   num = Math.floor(num / 10);
 }
-
 console.log(prod);
+
+// Возведение в степень
+prod **= 3;
+console.log('prod: ', prod);
+
+//Вывод первых 2-ух цифр числа
+console.log(prod.toString().slice(0, 2));
