e594c7824edb2372601d8ed24d33b3a95c4d7c31
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2020-11-25 09:44:57 +0700

    feat hard lesson

--- script.js
+++ script.js
@@ -1,86 +1,22 @@
 'use strict';
 
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-let money = 76500,
-    income = 'Фриланс',
-    addExpenses = 'Бенизин, Интернет, Коммуналка',
-    deposit = true,
-    mission = 1000000,
-    period = 5,
-    expenses = [];
-
-let start = function() {
-    do {
-        money = prompt('Ваш месячный доход');
+let arr = [];
+//Задание 1
+for (let i = 0; i < 7; i++) {
+    arr[i] = prompt('Введите любое многозначное число');
+    if (arr[i][0] === '2' || arr[i][0] === '4') {
+        console.log(arr[i]);
     }
-    while (!isNumber(money));
-};
+}
 
-start();
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Бенизин, Интернет, Коммуналка');
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-let getExpensesMonth = function() {
-    let sum = 0;
-    let amount;
-    for (let i = 0; i < 2; i++) {
-        expenses[i] = prompt('Введите статью расходов');
-        amount = prompt('Во сколько это обойдется?');
-        while (!isNumber(amount)) {
-            amount = prompt('Во сколько это обойдется?');
+//Задание 2
+param:
+    for (let i = 2; i <= 100; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                continue param;
+            }
         }
-        sum += +amount;
+        console.log(i + ' - просто число, делится только на 1 и на ' + i);
+
     }
-    return sum;
-};
-
-let expensesAmount = getExpensesMonth();
-
-const showTypeOf = function(data) {
-    console.log(typeof(data));
-};
-
-const getAccumulatedMonth = function() {
-    return money - expensesAmount;
-};
-
-let accumulatedMonth = getAccumulatedMonth();
-
-const getTargetMonth = function() {
-    if (mission / accumulatedMonth < 0) {
-        return ('Цель не будет достигнута');
-    }
-    return ('Цель будет достигнута за: ' + Math.ceil(mission / accumulatedMonth) + ' месяцев')
-};
-
-let budgetDay = accumulatedMonth / 30;
-
-const getStatusIncome = function(budgetDay) {
-    if (budgetDay >= 1200) {
-        return ('У вас высокий уровень дохода');
-    } else if (budgetDay >= 600 && budgetDay < 1200) {
-        return ('У вас средний уровень дохода');
-    } else if (budgetDay < 600 && budgetDay > 0) {
-        return ('К сожалению у вас уровень дохода ниже среднего');
-    } else {
-        return ('Что то пошло не так');
-    }
-};
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-console.log('Расходы за месяц: ' + expensesAmount);
-
-console.log('Возможные расходы: ', addExpenses.toLowerCase().split(", "));
-
-console.log(getTargetMonth());
-
-console.log('Бюджет на день: ', Math.floor(budgetDay));
-
-console.log(getStatusIncome(budgetDay));
