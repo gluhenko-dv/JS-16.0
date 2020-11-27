79c02b0da60169fb8a326e9d470f529ef177d3d6
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2020-11-27 13:26:15 +0700

    feat switch

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ let date = new Date();
 
 document.body.innerHTML += week + `</br>`;
 
-for (let i = 0; i < week.length; i++){
-  if(i + 1 === date.getDay()){
+for (let i = 0; i < week.length; i++) {
+  if (i + 1 === date.getDay()) {
     document.body.innerHTML += `</br> <strong>` + week[i];
   } else if (i === 5 || i === 6) {
-  document.body.innerHTML += `</br><i>` + week[i];
-  }else{
-  document.body.innerHTML += `</br>` + week[i];
+    document.body.innerHTML += `</br> <i>` + week[i];
+  } else {
+    document.body.innerHTML += `</br>` + week[i];
+  }
+}
+document.body.innerHTML += `<br>`;
+
+for (let i = 0; i < week.length; i++) {
+  switch (true) {
+    case (i + 1 === date.getDay()):
+      document.body.innerHTML += `</br> <strong>` + week[i];
+      break;
+    case (i === 5 || i === 6):
+      document.body.innerHTML += `</br> <i>` + week[i];
+      break;
+    default:
+      document.body.innerHTML += `</br>` + week[i];
   }
 }
