47ef67e659db87bf8c9d50887e616f5e54f66f64
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2020-11-27 07:01:13 +0700

    lesson6 with case

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
 'use strict';
 
-let gameStart = function() {
-    let gameNumber = Math.floor((Math.random() * 100) + 1),
-        userNumber,
-        attNumber = 10,
-        restart;
+const gameStart = function () {
+  let gameNumber = Math.floor((Math.random() * 100) + 1),
+    userNumber,
+    count = 10,
+    restart;
 
-    let gameRestart = function(restart) {
-        if (restart) {
-            return gameStart();
-        } else {
-            return alert('C тобой было круто! Пока-пока!');
-        }
-    };
+  const gameRestart = function () {
+    if (restart) {
+      return gameStart();
+    } else {
+      return alert('C тобой было круто! Пока-пока!');
+    }
+  };
 
-    let gameCheckNumber = function() {
-        userNumber = prompt('Угадай число от 1 до 100');
-        if (attNumber <= 10 && attNumber > 1) {
-            if (userNumber === null) {
-                restart = confirm('Сыграем еще разок?');
-                gameRestart(restart);
-            } else if (userNumber.replace(/\d/g, '') || !userNumber) {
-                alert('Введи число! А не кракозябру!!!');
-                gameCheckNumber();
-            } else if (userNumber > 100) {
-                alert('Ты не борщи! Твоё число больше 100! Давай по новой!');
-                gameCheckNumber();
-            } else if (userNumber < 1) {
-                alert('Ну ты чего? Твоё число меньше 1! Я так не играю! Давай по новой!');
-                gameCheckNumber();
-            } else if (userNumber > gameNumber) {
-                attNumber--;
-                alert('Многовато предлагаешь! Число меньше! Кол-во попыток: ' + attNumber);
-                gameCheckNumber();
-            } else if (userNumber < gameNumber) {
-                attNumber--;
-                alert('Маловато! Я загадал число больше! Кол-во попыток: ' + attNumber);
-                gameCheckNumber();
-            } else {
-                alert('ОПА! А ты угадал!!! Моё почтение :) Еще разок?');
-                gameStart();
-            }
-        } else {
-            restart = confirm('Попытки кончились! Сыграем еще разок?');
-            gameRestart(restart);
-        }
-    };
+  const gameCheckNumber = function () {
+    userNumber = prompt('Угадай число от 1 до 100');
+    if (count <= 10 && count > 1) {
+      switch (true) {
+        case (userNumber === null):
+          restart = confirm('Сыграем еще разок? :)');
+          gameRestart();
+          break;
+        case (isNaN(Number(userNumber)) || !userNumber):
+          alert('Введи число! А не кракозябру!!!');
+          gameCheckNumber();/* falls through */
+        case (Number(userNumber) > 100):
+          alert('Ты не борщи! Твоё число больше 100! Давай по новой!');
+          gameCheckNumber();/* falls through */
+        case (Number(userNumber) < 1):
+          alert('Ну ты чего? Твоё число меньше 1! Я так не играю! Давай по новой!');
+          gameCheckNumber();/* falls through */
+        case (Number(userNumber) > gameNumber):
+          count--;
+          alert('Многовато предлагаешь! Число меньше! Кол-во попыток: ' + count);
+          gameCheckNumber();/* falls through */
+        case (Number(userNumber) < gameNumber):
+          count--;
+          alert('Маловато! Я загадал число больше! Кол-во попыток: ' + count);
+          gameCheckNumber();/* falls through */
+        case (Number(userNumber) === gameNumber):
+          restart = confirm('ОПА! А ты угадал!!! Моё почтение :) Еще разок?');
+          gameRestart();
+          break;
+      }
+    } else {
+      restart = confirm('Попытки кончились! Сыграем еще разок?');
+      gameRestart();
+    }
+  };
 
-    gameCheckNumber();
+  gameCheckNumber();
 
 };
 
