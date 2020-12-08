36b2b40d5fc3b697b183227d78330e351a0d5d2b
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2020-12-08 17:32:44 +0700

    fix

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
 'use strict';
-document.addEventListener("DOMContentLoaded", function () {
 
 
-  const body = document.querySelector('body');
 
-  function DomElement(selector, height, width, bg, fontSize, text, position) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.text = text;
-    this.position = position;
+const body = document.querySelector('body');
+
+function DomElement(selector, height, width, bg, fontSize, text, position) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+  this.text = text;
+  this.position = position;
+}
+
+DomElement.prototype.createElem = function () {
+  switch (true) {
+    case (this.selector[0] === '.'):
+      body.innerHTML += `<div class="${this.selector.substr(1)}" ></div>`;
+      break;
+    case (this.selector[0] === '#'):
+      body.innerHTML += `<p id="${this.selector.substr(1)}"></p>`;
+      break;
+    default:
+      body.innerHTML += `<div class="${this.selector}" ></div>`;
+      break;
   }
+  const elem = document.querySelector(this.selector);
+  elem.style.height = `${this.height}px`;
+  elem.style.width = `${this.width}px`;
+  elem.style.backgroundColor = this.bg;
+  elem.style.fontSize = `${this.fontSize}px`;
+  elem.textContent = this.text;
+  elem.style.position = this.position;
 
-  DomElement.prototype.createElem = function () {
-    switch (true) {
-      case (this.selector[0] === '.'):
-        this.selector = this.selector.substr(1);
-        body.innerHTML = `<div class="${this.selector}" ></div>`;
-        const div = document.querySelector('.' + this.selector);
-        div.style.height = `${this.height}px`;
-        div.style.width = `${this.width}px`;
-        div.style.backgroundColor = this.bg;
-        div.style.fontSize = `${this.fontSize}px`;
-        div.textContent = this.text;
-        break;
-      case (this.selector[0] === '#'):
-        this.selector = this.selector.substr(1);
-        body.innerHTML = `<p id="${this.selector}"></p>`;
-        const p = document.querySelector('#' + this.selector);
-        p.style.height = `${this.height}px`;
-        p.style.width = `${this.width}px`;
-        p.style.backgroundColor = this.bg;
-        p.style.fontSize = `${this.fontSize}px`;
-        p.textContent = this.text;
-        break;
-    }
+};
+let obj1 = new DomElement(prompt('Введите selector'), prompt('Введите height'), prompt('Введите width'), prompt('Введите background color'), prompt('Введите fontSize'), prompt('Введите текст для блока'));
 
+obj1.createElem();
 
-  };
-  let obj1 = new DomElement(prompt('Введите selector'), prompt('Введите height'), prompt('Введите width'), prompt('Введите background color'), prompt('Введите fontSize'), prompt('Введите текст для блока'));
+let obj2 = new DomElement('.selector', '100', '100', 'red', '14', ' ', 'absolute');
 
-  obj1.createElem();
-
-  let obj2 = new DomElement({width: '100px'}, {height: '100px'}, {color: 'red'}, {position: 'absolute'});
-
+document.addEventListener("DOMContentLoaded", function () {
   obj2.createElem();
-
 });
