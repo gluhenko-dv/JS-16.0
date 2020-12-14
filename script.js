a3d7c3f58a548f2ee4534915b14c6c3fd9c177f1
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2020-12-14 15:52:32 +0700

    lesson18 hard2

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 "use sctrict";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const paragraph = document.querySelector(".paragraph"),
-    input = document.querySelector(".input");
-  const output = () => {
-    paragraph.textContent = input.value;
+  const progress = document.querySelector(".progress"),
+    btn = document.querySelector(".btn"),
+    reset = document.querySelector(".reset");
+  let animate,
+    checkAnimate = false;
+  let width = 1;
+  const load = () => {
+    animate = requestAnimationFrame(load);
+    if (width < 100) {
+      progress.style.width = `${width}%`;
+      width++;
+    } else {
+      cancelAnimationFrame(animate);
+    }
   };
-  input.addEventListener("change", () => {
-    setTimeout(output, 300);
+
+  btn.addEventListener("click", () => {
+    if (!checkAnimate) {
+      checkAnimate = true;
+      animate = requestAnimationFrame(load);
+    } else {
+      checkAnimate = false;
+      cancelAnimationFrame(animate);
+    }
+  });
+  reset.addEventListener("click", () => {
+    width = 1;
+    progress.style.width = `${width}%`;
   });
 });
