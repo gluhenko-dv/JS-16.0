c91f38c97116ec6d9c7741f8dc375391cde4cd22
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2020-12-16 15:07:33 +0700

    feat tabs & update toggleMenu()

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-parens */
 "use sctrict";
 window.addEventListener("DOMContentLoaded", () => {
   //hero timer
@@ -45,45 +46,48 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //главное меню
   const toggleMenu = () => {
-    const btnMenu = document.querySelector(".menu"),
-      menu = document.querySelector("menu"),
-      btnCloseMenu = document.querySelector(".close-btn"),
-      menuItems = menu.querySelectorAll("ul>li");
+    const menu = document.querySelector("menu"),
+      body = document.querySelector("body");
 
     const handlerMenu = () => {
       menu.classList.toggle("active-menu");
     };
 
-    btnMenu.addEventListener("click", handlerMenu);
-    btnCloseMenu.addEventListener("click", handlerMenu);
-    menuItems.forEach((element) =>
-      element.addEventListener("click", handlerMenu)
-    );
-  };
-  toggleMenu();
-  //menu scrollIntoView
-  const smoothScroll = () => {
-    const anchors = document.querySelectorAll('a[href*="#"');
-
-    anchors.forEach((element) => {
-      element.addEventListener("click", (event) => {
-        event.preventDefault();
-        const blockId = element.getAttribute("href");
-        document.querySelector("" + blockId).scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
+    //menu scrollIntoView
+    const smoothScroll = (target) => {
+      const blockId = target.getAttribute("href");
+      document.querySelector("" + blockId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
       });
+    };
+
+    body.addEventListener("click", (event) => {
+      const target = event.target;
+
+      if (target.closest(".menu")) {
+        handlerMenu();
+      } else if (target.closest(".close-btn")) {
+        handlerMenu();
+      } else if (target.closest("menu>ul>li")) {
+        handlerMenu();
+        event.preventDefault();
+        smoothScroll(target);
+      } else if (
+        !target.closest(".active-menu") &&
+        menu.classList.contains("active-menu")
+      ) {
+        handlerMenu();
+      }
     });
   };
-  smoothScroll();
+  toggleMenu();
+
   //popup
 
   const togglePopup = () => {
     const popup = document.querySelector(".popup"),
-      popupBtn = document.querySelectorAll(".popup-btn"),
-      popUpClose = document.querySelector(".popup-close");
-
+      popupBtn = document.querySelectorAll(".popup-btn");
     //popup animate
 
     let animate,
@@ -106,12 +110,45 @@ window.addEventListener("DOMContentLoaded", () => {
         }
       });
     });
-    popUpClose.addEventListener("click", () => {
-      popup.style.display = "none";
-      opacity = 0;
-      cancelAnimationFrame(animate);
+    popup.addEventListener("click", (event) => {
+      let target = event.target;
+      target = target.closest(".popup-content");
+      if (!target || event.target.matches(".popup-close")) {
+        popup.style.display = "none";
+        opacity = 0;
+      }
     });
   };
 
   togglePopup();
+
+  //табы
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header"),
+      tab = tabHeader.querySelectorAll(".service-header-tab"),
+      tabContent = document.querySelectorAll(".service-tab");
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tabContent[i].classList.add("d-none");
+          tab[i].classList.remove("active");
+        }
+      }
+    };
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+      target = target.closest(".service-header-tab");
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+  tabs();
 });
