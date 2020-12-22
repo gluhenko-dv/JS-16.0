ebb22b268ebbdd7deb5dc428ca95312a5b7abb55
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2020-12-22 16:19:04 +0700

    feat calc

--- scripts/script.js
+++ scripts/script.js
@@ -270,10 +270,12 @@ window.addEventListener("DOMContentLoaded", () => {
     return str.replace(/\D/g, "");
   };
   //калькулятор
-  const calc = () => {
+  const calc = (price = 100) => {
     const calcSquare = document.querySelector(".calc-square"),
       calcCount = document.querySelector(".calc-count"),
+      calcType = document.querySelector(".calc-type"),
       calcDay = document.querySelector(".calc-day"),
+      totalValue = document.getElementById("total"),
       calcBlock = document.querySelector(".calc-block");
     calcBlock.addEventListener("input", (e) => {
       target = e.target;
@@ -281,7 +283,51 @@ window.addEventListener("DOMContentLoaded", () => {
       if (!item) return;
       item.value = strWithNum(item.value);
     });
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
 
+      let animate,
+        countAnimate = 0;
+
+      const typeValue = calcType.options[calcType.selectedIndex].value;
+      console.log("typeValue: ", typeValue);
+      const squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      const calcAnimate = () => {
+        animate = requestAnimationFrame(calcAnimate);
+        if (countAnimate <= total) {
+          totalValue.textContent = countAnimate;
+          countAnimate += 3;
+        } else {
+          cancelAnimationFrame(animate);
+          totalValue.textContent = total;
+        }
+      };
+      animate = requestAnimationFrame(calcAnimate);
+
+    };
+    calcBlock.addEventListener("change", (e) => {
+      const target = e.target;
+      if (target.matches("select") || target.matches("input")) {
+        countSum();
+      }
+    });
   };
-  calc();
+  calc(100);
 });
